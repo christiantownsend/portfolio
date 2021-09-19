e58a57314882d3f279cf88361e22f01c2e8395eb
Christian Townsend <christianttownsend@gmail.com>
2021-09-18 23:18:48 -0400

    Switched to vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Christian Townsend Portfolio',
+    description: 'Just playing around',
+    markdown: {
+        anchor: { permalink: false, permalinkBefore: true, permalinkSymbol: '#' }
+    },
+}
